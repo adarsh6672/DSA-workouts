9983ecd3ccf970b50bb6e988ed77df8d95fe3858
adarsh6672 <adarshkeruvatty@gmail.com>
2023-12-15 16:03:56 +0530

    bst

--- practice/tree/BinarySearchTree.java
+++ practice/tree/BinarySearchTree.java
@@ -75,15 +75,121 @@ public class BinarySearchTree {
         inOrder(node.right);
     }
 
+    public Node delete(int key,Node root){
+        if(root==null){
+            return root;
+        }
+
+        if(key<root.data){
+            root.left=delete(key, root.left);
+            return root;
+        }else if(key>root.data){
+            root.right=delete(key, root.right);
+            return root;
+        }
+
+        if(root.left==null){
+            return root.right;
+        }else if(root.right==null){
+            return root.left;
+        }else{
+            Node succParent=root;
+            Node succ=root.right;
+
+            while(succ.left!=null){
+                succParent=succ;
+                succ=succ.left;
+            }
+
+            if(succParent!=root){
+                succParent.left =succ.right;
+            }else{
+                succParent.right=succ.right;
+            }
+
+            root.data=succ.data;
+
+            return root;
+        }
+    }
+
+    public Node deleteRec(int data, Node root){
+        if(root==null){
+            return root;
+        }
+
+        if(data<root.data){
+            root.left=deleteRec(data, root.left);
+            return root;
+        }else if(data> root.data){
+            root.right=deleteRec(data, root.right);
+            return root;
+        }
+
+        if(root.left==null){
+            return root.right;
+        }else if(root.right==null){
+            return root.left;
+        }else{
+            Node sp=root;
+            Node s=root.right;
+            while(s.left!=null){
+                sp=s;
+                s=s.left;
+            }
+
+            if(sp!=root){
+                sp.left=s.right;
+            }else{
+                sp.right=s.right;
+            }
+
+            root.data=s.data;
+            return root;
+        }
+    }
+
+    public void postOrder(Node node){
+        if(node==null){
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.data+"   ");
+    }
+
+    public void preOrder(Node node){
+        if(node == null){
+            return;
+        }
+        System.out.print(node.data+"  ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
     public static void main(String[] args) {
         BinarySearchTree bst=new BinarySearchTree();
         bst.insert(10);
         bst.insert(5);
-        bst.insert(22); 
-        bst.insert(88);
-        System.out.println(bst.contains(5));
-        System.out.println(bst.search(10, bst.root));
+        bst.insert(3);
+        bst.insert(6);
+        bst.insert(1);
+        bst.insert(4);
+        bst.insert(20);
+        bst.insert(25);
+        bst.insert(19);
+        bst.insert(26);
+
+    
+        System.out.println(bst.root.data);
         bst.inOrder(bst.root);
+        System.out.println();
         
+        System.out.println(bst.root.data);
+        bst.inOrder(bst.root);
+        System.out.println();
+        bst.postOrder(bst.root);
+        System.out.println();
+        bst.preOrder(bst.root);
     }
 }
